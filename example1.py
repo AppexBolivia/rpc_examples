41be0952993ce97be4f09c585850119683087b34
Felix Daniel Coca Calvimontes <daniel.uremix@gmail.com>
2023-07-10 23:25:56 -0400

    Add first examples to repo

---
+++ example1.py
@@ -0,0 +1,22 @@
+# odoo rpc example
+
+import xmlrpc.client
+
+# Login to Odoo server
+url = 'http://localhost:8069'
+db = 'v15'
+username = 'admin'
+password = 'admin'
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+uid = common.authenticate(db, username, password, {})
+print("Logged in as %s (uid: %d)" % (username, uid))
+
+# Read the sessions
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+# get account.move that are not invoices
+domain = [('move_type', '=', 'entry')]
+fields = ['name', 'date', 'ref', 'journal_id', 'line_ids']
+move_ids = models.execute_kw(db, uid, password, 'account.move', 'search_read', [domain], {'fields': fields})
+for move in move_ids:
+    print(move)
+
---
+++ example2.py
@@ -0,0 +1,37 @@
+import psycopg2
+
+# Login to Odoo server
+url = 'http://localhost:8069'
+db = 'v15'
+username = 'test'
+password = 'test'
+conn = psycopg2.connect(dbname=db, user=username, password=password, host='localhost')
+cur = conn.cursor()
+query = """
+        SELECT
+            move.name AS move_name,
+            move.date AS move_date,
+            move.ref AS move_ref,
+            move.journal_id AS journal_id,
+            array_agg(line.id) AS line_ids
+        FROM
+            account_move move
+        INNER JOIN
+            account_move_line line ON line.move_id = move.id
+        GROUP BY
+            move.id
+    """
+
+# Execute the query
+cur.execute(query)
+# Fetch the results
+move_ids = cur.fetchall()
+# 'name', 'date', 'ref', 'journal_id', 'line_ids'
+for move in move_ids:
+    print({
+        'name': move[0],
+        'date': move[1],
+        'ref': move[2] or False,
+        'journal_id': move[3],
+        'line_ids': move[4]
+    })
